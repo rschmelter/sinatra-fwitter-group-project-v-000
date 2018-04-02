e055cbe1b68b27e892767558e28a2b273d69da0d
Ryan Schmelter <rschmelter33@gmail.com>
2018-04-02 23:26:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    use Rack::Session::Cookie,  :key => 'rack.session',
+                                :path => '/',
+                                :secret => 'your_secret'
+
   end
+
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 
 end
